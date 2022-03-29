ea0be951afef39d4f1b236f457aa843d8025405f
ivanvolkov <volkov.ivan20032012@yandex.ru>
2022-03-29 16:59:26 +0300

    Добавил работу с моделями в контроллеры

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from django.shortcuts import render
 
+# подключили наши модельки(таблицы в б.д), чтобы добавить их в контроллеры
+from mainapp.models import Product, ProductCategory
+
 # Create your views here.
 
 
@@ -30,62 +33,19 @@ def index(request):
 
 
 def products(request):
+    # соответственно нам надо передать в переменную данные из таблицы
+    categories = ProductCategory.objects.all()
+    # тянем из базы для карточек товаров
+    cards = Product.objects.all()
+
     content = {
         # титл страницы products
         'title': 'GeekShop - Каталог',
         # категории товаров
-        'categories': [
-            {'name': 'Новинки'},
-            {'name': 'Одежда'},
-            {'name': 'Обувь'},
-            {'name': 'Аксессуары'},
-            {'name': 'Подарки'},
-        ],
+        'categories': categories,
         # карточки товаров
-        'cards': [
-            {
-             'name': 'Худи черного цвета с монограммами adidas Originals',
-             'price': '6 090,00 руб.',
-             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-             'basket': 'Отправить в корзину',
-             'link': 'vendor/img/products/Adidas-hoodie.png'
-            },
-            {
-              'name': 'Синяя куртка The North Face',
-              'price': '23 725,00 руб.',
-              'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-              'basket': 'Отправить в корзину',
-              'link': 'vendor/img/products/Blue-jacket-The-North-Face.png'
-            },
-            {
-              'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-              'price': '3 390,00 руб.',
-              'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-              'basket': 'Отправить в корзину',
-              'link': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'
-            },
-            {
-              'name': 'Черный рюкзак Nike Heritage',
-              'price': '2 340,00 руб.',
-              'description': 'Плотная ткань. Легкий материал.',
-              'basket': 'Отправить в корзину',
-              'link': 'vendor/img/products/Black-Nike-Heritage-backpack.png'
-            },
-            {
-              'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-              'price': '13 590,00 руб.',
-              'description': 'Гладкий кожаный верх. Натуральный материал.',
-              'basket': 'Отправить в корзину',
-              'link': 'vendor/img/products/Black-Dr-Martens-shoes.png'
-            },
-            {
-              'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-              'price': '2 890,00 руб.',
-              'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-              'basket': 'Отправить в корзину',
-              'link': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'
-            }
-        ],
+        'cards': cards,
+        # предыдущие товары
         'previous': 'Previous',
         # страницы
         'pages': [1, 2, 3],
