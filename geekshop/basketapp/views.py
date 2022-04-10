34aac2fecda596b90db5d50a9e37069639ced167
ivanvolkov <volkov.ivan20032012@yandex.ru>
2022-04-10 20:48:20 +0300

    views.py

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,27 +1,68 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
-from mainapp.models import Product
-from basketapp.models import Basket
-from django.http.response import HttpResponseRedirect
+
 # Create your views here.
+from django.template.loader import render_to_string
+
+from basketapp.models import Basket
+from mainapp.models import Product
+
+# @login_required
+# def basket_add(request,id):
+#     user_select = request.user
+#     product = Product.objects.get(id=id)
+#     baskets = Basket.objects.filter(user=user_select,product=product)
+#
+#     if baskets:
+#         basket = baskets.first()
+#         basket.quantity +=1
+#         basket.save()
+#     else:
+#         Basket.objects.create(user=user_select,product=product,quantity=1)
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-# для добавления товара в корзину
+@login_required
 def basket_add(request, id):
-    user_select = request.user
-    product = Product.objects.get(id=id)
-    baskets = Basket.objects.filter(user=user_select, product=product)
+    if request.is_ajax():
+        user_select = request.user
+        product = Product.objects.get(id=id)
+        baskets = Basket.objects.filter(user=user_select, product=product)
 
-    # если корзина существует
-    if baskets:
-        basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
-    else:
-        Basket.objects.create(user=user_select, product=product, quantity=1)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        if baskets:
+            basket = baskets.first()
+            basket.quantity += 1
+            basket.save()
+        else:
+            Basket.objects.create(user=user_select, product=product, quantity=1)
+
+        products = Product.objects.all()
+        context = {'products': products}
+
+        result = render_to_string('mainapp/includes/cards.html', context)
+        return JsonResponse({'result': result})
 
 
-# для удаления товара из корзины
+@login_required
 def basket_remove(request, basket_id):
     Basket.objects.get(id=basket_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, basket_id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=basket_id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user = request.user)
+        context = {'baskets':baskets}
+
+        result = render_to_string('basketapp/basket.html',context)
+        return JsonResponse({'result':result})
+
