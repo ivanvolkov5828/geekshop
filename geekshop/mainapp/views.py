59b975386add4721bb08509e1a09922e5474b5e4
admin <volkov.ivan20032012@yandex.ru>
2022-05-29 22:28:23 +0300

    lesson_14_update_1

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -14,9 +14,9 @@ def index(request):
 def products(request, id_category=None, page=1):
 
     if id_category:
-        products = Product.objects.filter(category_id=id_category)
+        products = Product.objects.filter(category_id=id_category).select_related()
     else:
-        products = Product.objects.all()
+        products = Product.objects.all().select_related('category')
 
     pagination = Paginator(products, per_page=3)
 
