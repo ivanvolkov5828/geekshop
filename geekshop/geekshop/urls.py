c53261945cf4eb20415c1c0f0cf362ba5bea7014
admin <volkov.ivan20032012@yandex.ru>
2022-05-29 17:22:49 +0300

    lesson_14_update

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-
+import debug_toolbar
 
 from django.contrib import admin
 from django.urls import path, include
@@ -35,5 +35,5 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += [path('debug/', include(debug_toolbar.urls))]
 
